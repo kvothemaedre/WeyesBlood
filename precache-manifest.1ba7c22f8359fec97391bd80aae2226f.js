f2120c0adffa5fb956b696f53cf12b1fc41a05a8
Darshan C <darshansagar.chudiwal2018@vitstudent.ac.in>
2020-07-16 00:08:36 +0530

    Updates

---
+++ precache-manifest.1ba7c22f8359fec97391bd80aae2226f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9228f496ce4f37340ee1f3567f504fd",
+    "url": "/WeyesBlood/index.html"
+  },
+  {
+    "revision": "44a62f4ced03b2ae95ab",
+    "url": "/WeyesBlood/static/js/2.fbf96fe8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WeyesBlood/static/js/2.fbf96fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6313c74d23db454458c6",
+    "url": "/WeyesBlood/static/js/main.540f7166.chunk.js"
+  },
+  {
+    "revision": "b6ddbef8226d6516d854",
+    "url": "/WeyesBlood/static/js/runtime-main.927d3e6c.js"
+  },
+  {
+    "revision": "f37dca744d35ff03fd81abc68e77fde8",
+    "url": "/WeyesBlood/static/media/AboutWeyes.f37dca74.svg"
+  },
+  {
+    "revision": "aa2753f381c23890e6fafa377a4d92aa",
+    "url": "/WeyesBlood/static/media/Caroline-Polachek.aa2753f3.svg"
+  },
+  {
+    "revision": "8b1c6ca7c04baf5abcc92b7fef080a0b",
+    "url": "/WeyesBlood/static/media/Half-Waif.8b1c6ca7.svg"
+  },
+  {
+    "revision": "269965d8f17b91b30ea0702ada5655e4",
+    "url": "/WeyesBlood/static/media/HeaderWeyes.269965d8.svg"
+  },
+  {
+    "revision": "819729fea3a517b173ff06adc3d44dd3",
+    "url": "/WeyesBlood/static/media/PerformingWeyes.819729fe.svg"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/WeyesBlood/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "8c0b47f86e91ee004d4d48f3da81eb53",
+    "url": "/WeyesBlood/static/media/WeyesFloat.8c0b47f8.png"
+  },
+  {
+    "revision": "70e5b5d443953d2144bb9bf1d4884286",
+    "url": "/WeyesBlood/static/media/WeyesWater.70e5b5d4.gif"
+  },
+  {
+    "revision": "4b474e64447acbf463919187e5e7f261",
+    "url": "/WeyesBlood/static/media/julia-holter.4b474e64.svg"
+  }
+]);
